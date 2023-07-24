0ad13eaf8ff89e67cde383171c944fa7e701f3d0
Sigmacci <mamchur.paviel@gmail.com>
2023-07-24 11:17:11 +0200

    Added 'Edit profile' UI and loading screen

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -52,7 +52,7 @@ export class MemberDetailComponent implements OnInit {
     this.memberService.getMember(username).subscribe({
       next: member => {
         this.member = member;
-        this.galleryImages = this.getImages();
+        this.galleryImages = this.getImages()
       }
     })
   }
